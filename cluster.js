d386415ad1857488c35a790dd03c166d028b2e53
Ericson Weah <ericson.weah@gmail.com>
2023-03-12 23:51:07 -0600

    refractored started

---
+++ cluster.js
@@ -0,0 +1,36 @@
+const cluster = require('cluster');
+const os = require('os');
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+
+process.on('uncaughtException', function(err) {
+	console.log('UNHANDLED ERROR: ', err);
+	process.exit(1);
+});
+
+process.on('unhandledRejection', function(err) {
+	console.log('UNHANDLED PROMISE REJECTION: ', err);
+	process.exit(1);
+});
+
+
+if (cluster.isMaster) {
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork();
+  }
+} else {
+  const app = require('./app');
+  
+  // app.listen(3000);
+  // console.log(`Worker ${cluster.worker.id} running`);
+  module.exports = app.listen(PORT, HOST, function(err) {
+    if (err) {
+      throw err;
+    }
+    // console.log(`Server listening on port: ${HOST}:${PORT}`);
+    console.log(`Worker ${cluster.worker.id} running`);
+  });
+  
+}
--- old-app.js
+++ old-app.js
@@ -2,15 +2,11 @@ if (require.main === module) {
 	throw new Error('Do not run directly. Use server.js to start.');
 }
 
-
 const {join} = require('path')
 const Koa = require('koa');
 const render = require('koa-ejs');
 const bodyParser = require('koa-bodyparser');
 const json = require('koa-json');
-const redis = require('redis');
-const {Server} = require('http');
-const client = redis.createClient();
 
 
 const koaStatic = require('koa-static');
@@ -25,9 +21,11 @@ render(app, {
   layout: 'layout',
   viewExt: 'html',
   cache: false,
-  debug: false
+     debug: false
 });
  
+
+
 // Register Routes
 require('./routes')(app);
 
@@ -35,29 +33,4 @@ require('./routes')(app);
 // Serve static files from public directory
 app.use(koaStatic('./public'));
 
-const http = Server(app);
-const io = require('socket.io')(http);
-
-// const server = app.listen(3000);
-
-
-client.on('connect', () => {
-  console.log('Redis client connected');
-});
-
-client.on('error', (err) => {
-  console.log(`Error connecting to Redis: ${err}`);
-});
-
-io.on('connection', (socket) => {
-  console.log('Socket connected:', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('Socket disconnected:', socket.id);
-  });
-});
-
-module.exports = app
-
-
-
+module.exports = app;
