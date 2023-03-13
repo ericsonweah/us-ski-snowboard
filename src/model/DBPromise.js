8cab245c27d4ce575defab1412764cde76f3129d
Ericson Weah <ericson.weah@gmail.com>
2023-03-13 09:31:05 -0600

    DBPromise refractored

--- src/model/DBPromise.js
+++ src/model/DBPromise.js
@@ -50,6 +50,18 @@ class DBPromise extends require("../Base") {
     return process.cwd() + path
   }
 
+
+   /**
+   * @name all
+   * @function
+   *
+   * @param {String} table the database table name
+   *
+   * @description makes an https get request to an api endpoint
+   *
+   * @return {Array|Object} the data from the database 
+   *
+   */
   async all(table = this.table || 'members') {
     const records = await new Promise((resolve, reject) => {
       db.all(`SELECT * FROM ${table}`, (err, rows) => {
@@ -60,6 +72,18 @@ class DBPromise extends require("../Base") {
     return records;
   }
 
+
+  /**
+   * @name membersDetails
+   * @function
+   *
+   * @param {String} table the database table name
+   *
+   * @description makes an https get request to an api endpoint
+   *
+   * @return {Array|Object} all members with their addresses
+   *
+   */
   async membersDetails() {
     const records = await new Promise((resolve, reject) => {
       db.all(`SELECT * FROM members INNER JOIN addresses ON members.id = addresses.memberId`, (err, rows) => {
@@ -70,6 +94,18 @@ class DBPromise extends require("../Base") {
     return records;
   }
 
+
+    /**
+   * @name dropTable
+   * @function
+   *
+   * @param {String} table the database table name
+   *
+   * @description makes an https get request to an api endpoint
+   *
+   * @return {Object|Boolean} results of droping a table
+   *
+   */
   async dropTable(table = this.table) {
     const records = await new Promise((resolve, reject) => {
       db.all(`DROP ${table}`, (err, rows) => {
@@ -81,6 +117,18 @@ class DBPromise extends require("../Base") {
   }
 
 
+  /**
+   * @name firstCountMembersDetails
+   * @function
+   *
+   * @param {Integer} count quantity
+   *
+   * @description makes an https get request to an api endpoint
+   *
+   * @return {Object|Array}  all record found
+   *
+   */
+
   async firstCountMembersDetails(count = 10) {
     const records = await new Promise((resolve, reject) => {
       db.all(`SELECT DISTINCT * FROM members INNER JOIN addresses ON members.id = addresses.memberId LIMIT ${count}`, (err, rows) => {
@@ -90,6 +138,19 @@ class DBPromise extends require("../Base") {
     });
     return records;
   }
+
+
+  /**
+   * @name count
+   * @function
+   *
+   * @param {String} table database table name
+   *
+   * @description count records in the given table
+   *
+   * @return {Object|Array}  all record found
+   *
+   */
 
   async count (table = this.table || 'members') {
     const count = await new Promise((resolve, reject) => {
@@ -105,6 +166,21 @@ class DBPromise extends require("../Base") {
 
   }
 
+  /**
+   * @name findBy
+   * @function
+   *
+   * @param {String} column database table column name
+   * @param {String} value database table column value
+   * @param {String} success success event
+   * @param {String} error  error event
+   * @param {String} table database table name
+   *
+   * @description find record by column name and column value
+   *
+   * @return {Object|Array}  all record found
+   *
+   */
   async findBy(column = 'colum name', value = 'column value', success = 'success', error = 'error', table = this.table || 'members') {
     const records = await new Promise((resolve, reject) => {
       db.all(`SELECT * FROM ${table} WHERE ${column}='${value}'`, (err, rows) => {
@@ -119,6 +195,22 @@ class DBPromise extends require("../Base") {
     });
     return records;
   }
+
+
+     /**
+   * @name findByQuery
+   * @function
+   *
+   * @param {String} query database table column name
+   * @param {String} success success event
+   * @param {String} error  error event
+   * @param {String} table database table name
+   *
+   * @description find record by column name and column value
+   *
+   * @return {Object|Array}  all record found
+   *
+   */
   async findByQuery(query, success = 'success', error = 'error', table = this.table || 'members') {
     const records = await new Promise((resolve, reject) => {
       db.all(query, (err, rows) => {
@@ -133,9 +225,26 @@ class DBPromise extends require("../Base") {
     });
     return records;
   }
+
+
+  /**
+   * @name firstBy
+   * @function
+   *
+   * @param {String} column database table column name
+   * @param {String} value database table column value
+   * @param {String} success success event
+   * @param {String} error  error event
+   * @param {String} table database table name
+   *
+   * @description find first record by column name and column value
+   *
+   * @return {Object}  first record found
+   *
+   */
   async firstBy(column = 'colum name', value = 'column value', success = 'success', error = 'error', table = this.table || 'members') {
     const records = await new Promise((resolve, reject) => {
-      db.all(`SELECT * FROM ${table} WHERE ${column}='${value}' ORDER BY ${column} DESC LIMIT 1`, (err, rows) => {
+      db.get(`SELECT * FROM ${table} WHERE ${column}='${value}' ORDER BY ${column} DESC LIMIT 1`, (err, rows) => {
         if (err) {
           this.emit(error, err);
           reject(err);
@@ -148,6 +257,22 @@ class DBPromise extends require("../Base") {
     return records;
   }
 
+
+   /**
+   * @name lastBy
+   * @function
+   *
+   * @param {String} column database table column name
+   * @param {String} value database table column value
+   * @param {String} success success event
+   * @param {String} error  error event
+   * @param {String} table database table name
+   *
+   * @description find last record by column name and column value
+   *
+   * @return {Object}  last record found
+   *
+   */
   async lastBy(column = 'colum name', value = 'column value', success = 'success', error = 'error', table = this.table || 'members') {
     const records = await new Promise((resolve, reject) => {
       db.all(`SELECT * FROM ${table} WHERE ${column}='${value}' ORDER BY ${column} ASC LIMIT 1`, (err, rows) => {
@@ -163,6 +288,19 @@ class DBPromise extends require("../Base") {
     return records;
   }
 
+   /**
+   * @name findById
+   * @function
+   *
+   * @param {String} id table datum id
+   * @param {String} table database table name
+   *
+   * @description find last record by column name and column value
+   *
+   * @return {Object}  last record found
+   *
+   */
+
   async findById(id, table = this.table || 'members') {
     const member = await new Promise((resolve, reject) => {
       db.get(`SELECT * FROM members INNER JOIN addresses ON members.id = addresses.memberId WHERE members.id='${id}' LIMIT 1`, (err, rows) => {
@@ -172,126 +310,179 @@ class DBPromise extends require("../Base") {
     });
     return member;
   }
+
+  /**
+   * @name findByFirstName
+   * @function
+   *
+   * @param {String} firstName table datum firstName
+   * @param {String} table database table name
+   *
+   * @description finds records by first name
+   *
+   * @return {Object}  all records
+   *
+   */
   async findByFirstName(firstName, table = this.table || 'members') {
     return await this.findBy('firstName', firstName, 'findByFirstName', 'findByFirstName-error', table);
   }
 
+
+   /**
+   * @name findByLastName
+   * @function
+   *
+   * @param {String} lastName table datum lastName
+   * @param {String} table database table name
+   *
+   * @description finds records by last name
+   *
+   * @return {Object}  all records
+   *
+   */
+
   async findByLastName(lastName, table = this.table || 'members') {
     return await this.findBy('lastName', lastName, 'findByLastName', 'findByLastName-error', table);
   }
+
+  
+   /**
+   * @name findByPhone
+   * @function
+   *
+   * @param {String} phone table datum phone number
+   * @param {String} table database table name
+   *
+   * @description finds records by phone
+   *
+   * @return {Object}  all records
+   *
+   */
   async findByPhone(phone, table = this.table || 'members') {
     return await this.findBy('phone', phone, 'findByPhone', 'findByPhone-error', table);
   }
+
+
+   /**
+   * @name findByTitle
+   * @function
+   *
+   * @param {String} tile table datum title
+   * @param {String} table database table name
+   *
+   * @description finds records by title
+   *
+   * @return {Object}  all records
+   *
+   */
   async findByTitle(title, table = this.table || 'members') {
     return await this.findBy('title', title, 'findByTitle', 'findByTitle-error', table);
   }
 
+
+    /**
+   * @name findByZipCode
+   * @function
+   *
+   * @param {String} zip table datum zip
+   * @param {String} table database table name
+   *
+   * @description finds records by zip code
+   *
+   * @return {Object}  all records
+   *
+   */
+  
   async findByZipCode(zip = '84105', table = this.table || 'members') {
     return await this.findBy('zip', zip, 'findByZipCode', 'findByZipCode-error', table);
 
   }
+
+    /**
+   * @name findByCity
+   * @function
+   *
+   * @param {String} tile table datum city
+   * @param {String} table database table name
+   *
+   * @description finds records by city
+   *
+   * @return {Object}  all records
+   *
+   */
   async findByCity(city = 'Park City', table = this.table || 'members') {
     return await this.findBy('city', city, 'findByCity', 'findByCity-error', table);
 
   }
+
+    /**
+   * @name findByState
+   * @function
+   *
+   * @param {String} state table datum state
+   * @param {String} table database table name
+   *
+   * @description finds records by statee
+   *
+   * @return {Object}  all records
+   *
+   */
   async findByState(state = 'Utah', table = this.table || 'members') {
     return await this.findBy('state', state, 'findByState', 'findByState-error', table);
 
   }
+
+    /**
+   * @name findByAddress
+   * @function
+   *
+   * @param {String} address table datum address
+   * @param {String} table database table name
+   *
+   * @description finds records by address
+   *
+   * @return {Object}  all records
+   *
+   */
   async findByAddress(address = 'address', table = this.table || 'members') {
     return await this.findBy('address', address, 'findByAddress', 'findByAddress-error', table);
 
   }
 
+    /**
+   * @name findByCompany
+   * @function
+   *
+   * @param {String} company table datum company name
+   * @param {String} table database table name
+   *
+   * @description finds records by company
+   *
+   * @return {Object}  all records
+   *
+   */
   async findByCompany(company = 'US Ski', table = this.table || 'members') {
     return await this.findBy('company', company, 'findByCompany', 'findByCompany-error', table);
   }
+
+    /**
+   * @name findByUrl
+   * @function
+   *
+   * @param {String} url table datum url
+   * @param {String} table database table name
+   *
+   * @description finds records by url
+   *
+   * @return {Object}  all records
+   *
+   */
   async findByUrl(url = 'www.google.com', table = this.table || 'members') {
     return await this.findBy('url', url, 'findByUrl', 'findByUrl-error', table);
 
   }
 
-  // first 
-  async firstByFirstName(firstName = 'Darren', table = this.table || 'members') {
-    return await this.firstBy('firstName', firstName, 'firstByFirstName', 'firstByFirstName-error', table);
-  }
-  async firstByLastName(lastName = 'Johnston', table = this.table || 'members') {
-    return await this.firstBy('lastName', lastName, 'firstByLastName', 'firstByLastName-error', table);
-
-  }
-  async firstByZipCode(zip = '84105', table = this.table || 'members') {
-    return await this.firstBy('zip', zip, 'firstByZipCode', 'firstByZipCode-error', table);
-
-  }
-  async firstByCity(city = 'Park City', table = this.table || 'members') {
-    return await this.firstBy('city', city, 'firstByCity', 'firstByCity-error', table);
-
-  }
-  async firstByState(state = 'Utah', table = this.table || 'members') {
-    return await this.firstBy('state', state, 'firstByState', 'firstByState-error', table);
-
-  }
-  async firstByAddress(address = 'address', table = this.table || 'members') {
-    return await this.firstBy('address', address, 'firstByAddress', 'firstByAddress-error', table);
-
-  }
-  async firstByTitle(title = 'title', table = this.table || 'members') {
-    return await this.firstBy('title', title, 'firstByTitle', 'firstByTitle-error', table);
-
-  }
-  async firstByPhone(phone = 'phone', table = this.table || 'members') {
-    return await this.firstBy('phone', phone, 'firstByPhone', 'firstByPhone-error', table);
-
-  }
-  async firstByCompany(company = 'US Ski', table = this.table || 'members') {
-    return await this.firstBy('company', company, 'firstByCompany', 'firstByCompany-error', table);
-  }
-  async firstByUrl(url = 'www.google.com', table = this.table || 'members') {
-    return await this.firstBy('url', url, 'firstByUrl', 'firstByUrl-error', table);
-
-  }
-
-
-  // Last
-
-  async lastByFirstName(firstName = 'Darren', table = this.table || 'members') {
-    return await this.lastBy('firstName', firstName, 'lastByFirstName', 'lastByFirstName-error', table);
-  }
-  async lastByLastName(lastName = 'Johnston', table = this.table || 'members') {
-    return await this.lastBy('lastName', lastName, 'lastByLastName', 'lastByLastName-error', table);
-
-  }
-  async lastByZipCode(zip = '84105', table = this.table || 'members') {
-    return await this.lastBy('zip', zip, 'lastByZipCode', 'lastByZipCode-error', table);
-
-  }
-  async lastByCity(city = 'Park City', table = this.table || 'members') {
-    return await this.lastBy('city', city, 'lastByCity', 'lastByCity-error', table);
-
-  }
-  async lastByState(state = 'Utah', table = this.table || 'members') {
-    return await this.lastBy('state', state, 'lastByState', 'lastByState-error', table);
-
-  }
-  async lastByAddress(address = 'address', table = this.table || 'members') {
-    return await this.lastBy('address', address, 'lastByAddress', 'lastByAddress-error', table);
-
-  }
-  async lastByTitle(title = 'title', table = this.table || 'members') {
-    return await this.lastBy('title', title, 'lastByTitle', 'lastByTitle-error', table);
-
-  }
-  async lastByPhone(phone = 'phone', table = this.table || 'members') {
-    return await this.lastBy('phone', phone, 'lastByPhone', 'lastByPhone-error', table);
-
-  }
-  async lastByCompany(company = 'US Ski', table = this.table || 'members') {
-    return await this.lastBy('company', company, 'lastByCompany', 'lastByCompany-error', table);
-  }
-  async lastByUrl(url = 'www.google.com', table = this.table || 'members') {
-    return await this.lastBy('url', url, 'lastByUrl', 'lastByUrl-error', table);
-
-  }
+ 
 
 }
 
